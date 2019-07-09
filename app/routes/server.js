241efa1c2509d1e722a89316371935c94a8a9cf0
Ariel Gatica <arielgatica@agatica.agatica>
2019-07-09 09:27:25 -0400

    refactoring code

--- app/routes/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const apiRoutes = express.Router();
-const Logs = require('../controllers/controllers.logs');
-
-apiRoutes.route('/logs').get(Logs.listLogs);
-apiRoutes.route('/logs/watch').get(Logs.watchLogs);
-
-module.exports = apiRoutes;
