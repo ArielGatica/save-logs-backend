b7c9ba32c6403114397360af38f436a62fcb9bb2
Ariel Gatica Vargas <a.gatica1992@gmail.com>
2019-07-12 17:11:30 -0400

    Delete logs.js

--- api/models/logs.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-module.exports = mongoose.model('Logs', new Schema ({
-    logs: { type: String }
-}, { versionKey: false, timestamps: true }));
